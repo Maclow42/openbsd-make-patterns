9ebfdba09c8c73dfc5c42ed0e1705bbaa1242923
Thibault Colcomb <thibault.colcomb@hotmail.com>
2025-10-31 13:52:03 +0100

    [REMOVE] useless tranformation in code. Only commented to be able to restore it if needed but probably not

--- make/targ.c
+++ make/targ.c
@@ -292,11 +292,11 @@ Targ_BuildFromPattern(GNode *gn, char *pattern_value, size_t pattern_value_len)
 		// expand all $* repeatedly until no more found
 		char *expanded = emalloc(src_len + 1);
 		memcpy(expanded, src, src_len + 1);
-		while (strstr(expanded, "$*") != NULL) {
-			char *next = expand_from_char(expanded, strlen(expanded), "$*", 2, pattern_value, pattern_value_len);
-			free(expanded);
-			expanded = next;
-		}
+		// while (strstr(expanded, "$*") != NULL) {
+		// 	char *next = expand_from_char(expanded, strlen(expanded), "$*", 2, pattern_value, pattern_value_len);
+		// 	free(expanded);
+		// 	expanded = next;
+		// }
 
 		size_t elen = strlen(expanded);
 		struct command *new_cmd = emalloc(offsetof(struct command, string) + elen + 1);
